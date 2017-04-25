827ee6b7dcdbbc50b72393b1e4649a4134ed9a24
Naz Khan <nazmuskhan@yahoo.com>
2017-04-25 19:29:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+  function addBump(){
+    counter += 1
+  }
+  function getBumps(){
+    return counter
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function create(deadlyDevice){
+    return {animalType:animalType, deadlyDevice:deadlyDevice}
+  }return create
+}
+
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
